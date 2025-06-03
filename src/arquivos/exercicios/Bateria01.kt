7a26a4332d5bdd74ac6f20fe16dcf6ec308558b6
Ezequiel <ezequielsilvalima01@gmail.com>
2025-06-03 20:43:48 -0300

    commit

--- src/arquivos/exercicios/Bateria01.kt
+++ src/arquivos/exercicios/Bateria01.kt
@@ -4,7 +4,9 @@ fun main(){
     //soma()
     //calculoArea()
     //identificacao()
-    salario()
+    //salario()
+    celsiusToFahrenheit()
+    fahremheitToCelsius()
 }
 
 fun soma(){
@@ -47,10 +49,11 @@ fun identificacao(){
 }
 
 fun salario(){
+    //pag 140
     print("Digite o valor da hora-aula: ")
     val ha = readLine()!!.toDouble()
 
-    print("Digite o número de horas trabalhadas: ")
+    print("Digite o número de horas trabalhadas por mês: ")
     val ht = readLine()!!.toDouble()
 
     val pd = 0.2
@@ -65,4 +68,26 @@ fun salario(){
     println("Total do desconto: %.2f".format(td))
 
     println("Salário liquido: %.2f".format(sl))
+}
+
+
+
+fun celsiusToFahrenheit(){
+    //pag 146
+    //Formula: F ← C * 9 / 5 + 32
+    print("Digite a temperatura em graus Celsius: ")
+    val c = readLine()!!.toInt()
+
+    val f : Int = (c * 9 / 5) + 32
+
+    println("Temperatura em Fahrenheit: $f graus F")
+}
+
+fun fahremheitToCelsius(){
+    //C ← ((F – 32) * 5) / 9
+    print("Digite a temperatura em graus Fahrenheit: ")
+    val f = readLine()!!.toInt()
+
+    val c : Int = ((f - 32) * 5 / 9)
+    println("Temperatura em Celsius: $c graus C")
 }
